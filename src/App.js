232ae56b2af29f28903de88be2021df89f278e3d
Longle <letienlong12121998@gmail.com>
2022-06-03 11:29:05 +0700

    use hashrouter

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import {Fragment} from "react"
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
 import { publicRoutes } from './route/index.js';
 import DefaultLayout from './components/Layout/DefaultLayout/index.js';
